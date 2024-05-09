01f4b286700d0d376fe03cfb4c4846f0bb5208d7
Vitor Veras <vitorverasm@gmail.com>
2024-05-09 06:24:20 -0300

    fix: fix import

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import "react-native-gesture-handler";
 
 import { NavigationContainer } from "@react-navigation/native";
-import ThemeProvider from "./app/shared/layout/theme-provider";
+import ThemeProvider from "./app/shared/layout/ThemeProvider";
 import Router from "./app/shared/navigation/Router";
 
 export default function App() {
