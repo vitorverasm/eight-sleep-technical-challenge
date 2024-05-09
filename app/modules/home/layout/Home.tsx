41aeeea89be1eb5e065a9dc50a0a0a394276c11b
Vitor Veras <vitorverasm@gmail.com>
2024-05-09 19:27:43 -0300

    feat: fetch user sessions by userId

--- app/modules/home/layout/Home.tsx
+++ app/modules/home/layout/Home.tsx
@@ -4,9 +4,17 @@ import Header from "../../../shared/layout/components/Header";
 import Screen from "../../../shared/layout/components/Screen";
 import Title from "../../../shared/layout/components/Title";
 import { useProfile } from "../../profile-switcher/hooks/useProfile";
+import { useSessions } from "../../sessions/hooks/useSessions";
 
 function Home() {
   const { currentUser, signOutUser } = useProfile();
+  const { sessions, isLoading, error } = useSessions(currentUser?.id);
+
+  console.log("[Home] Response", {
+    sessions: sessions?.length,
+    isLoading,
+    error,
+  });
 
   return (
     <Screen>
