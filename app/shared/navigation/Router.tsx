ceb0f2d9f391a2e374327f18167ec746ce844504
Vitor Veras <vitorverasm@gmail.com>
2024-05-09 06:26:13 -0300

    feat: configure navigation Router

--- app/shared/navigation/Router.tsx
+++ app/shared/navigation/Router.tsx
@@ -1,5 +1,5 @@
 import { createStackNavigator } from "@react-navigation/stack";
-import Home from "../../modules/home/Home";
+import Home from "../../modules/home/layout/Home";
 import ProfileSwitcher from "../../modules/profile-switcher/layout/ProfileSwitcher";
 import { RootStackParamList } from "./types";
 import { useProfileSwitcherStore } from "../../modules/profile-switcher/state/useProfileSwitcherStore";
@@ -10,19 +10,20 @@ function Router() {
   const currentUser = useProfileSwitcherStore(state => state.currentUser);
 
   return (
-    <Stack.Navigator
-      initialRouteName={currentUser !== undefined ? "Home" : "ProfileSwitcher"}
-    >
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="ProfileSwitcher"
-        component={ProfileSwitcher}
-        options={{ headerShown: false }}
-      />
+    <Stack.Navigator>
+      {currentUser === undefined ? (
+        <Stack.Screen
+          name="ProfileSwitcher"
+          component={ProfileSwitcher}
+          options={{ headerShown: false }}
+        />
+      ) : (
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      )}
     </Stack.Navigator>
   );
 }
