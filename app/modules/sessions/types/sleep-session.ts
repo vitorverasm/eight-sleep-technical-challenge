7df9e14dfafa2cfdf80b166b98d047414c535d66
Vitor Veras <vitorverasm@gmail.com>
2024-05-09 18:52:57 -0300

    feat: add types for the sleep session object

---
+++ app/modules/sessions/types/sleep-session.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+import { SleepStageSchema } from "./sleep-stage";
+import { TimeSeriesSchema } from "./time-series";
+
+export const SleepSessionSchema = z.object({
+  /**
+   * The id of the interval
+   */
+  id: z.string({
+    required_error: "id is required",
+  }),
+  /**
+   * Time the session starts in ISO 8601 format
+   */
+  ts: z.string({
+    required_error: "id is required",
+  }),
+  /**
+   * The sleep score of the session (min 0, max 100)
+   */
+  score: z.number().min(0).max(100),
+  /**
+   * The sleep stages of the session
+   */
+  stages: SleepStageSchema.array(),
+  /**
+   * Collection of timeseries data
+   */
+  timeSeries: TimeSeriesSchema,
+});
+
+export type SleepSession = z.infer<typeof SleepSessionSchema>;
