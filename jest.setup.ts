6d439bff0ecc954450334a29fa1a620293569bbc
Vitor Veras <vitorverasm@gmail.com>
2024-05-10 17:38:35 -0300

    chore: setup base url for tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,6 @@
 import "@testing-library/react-native/extend-expect";
 import "react-native-gesture-handler/jestSetup";
 
-process.env.EXPO_PUBLIC_API_URL = "http://localhost";
+process.env.EXPO_PUBLIC_API_URL = "http://localhost:3333";
 
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
