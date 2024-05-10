97cd05bd07c911f3790d48df279c27e105c797e4
Vitor Veras <vitorverasm@gmail.com>
2024-05-10 04:50:10 -0300

    test: fix test errors

--- app/shared/tests/TestWrapper.tsx
+++ app/shared/tests/TestWrapper.tsx
@@ -1,6 +1,12 @@
+import { config } from "@gluestack-ui/config";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { NavigationContainer } from "@react-navigation/native";
 import { ReactNode } from "react";
 
 export function TestWrapper({ children }: { children: ReactNode }) {
-  return <NavigationContainer>{children}</NavigationContainer>;
+  return (
+    <GluestackUIProvider config={config} colorMode="dark">
+      <NavigationContainer>{children}</NavigationContainer>
+    </GluestackUIProvider>
+  );
 }
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,12 @@
 import "@testing-library/react-native/extend-expect";
 import "react-native-gesture-handler/jestSetup";
 
+jest.mock("./app/shared/config/env", () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      EXPO_PUBLIC_API_URL: "https://mockurl.com",
+    };
+  });
+});
+
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
