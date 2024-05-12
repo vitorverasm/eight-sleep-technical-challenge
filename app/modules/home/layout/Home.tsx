dd8a04c09790fe26fd6f6e67cbd056313d8c5b82
Vitor Veras <vitorverasm@gmail.com>
2024-05-12 05:40:30 -0300

    refactor: refactor home with new hooks

--- app/modules/home/layout/Home.tsx
+++ app/modules/home/layout/Home.tsx
@@ -1,20 +1,21 @@
 import { Box, Text } from "@gluestack-ui/themed";
-import React from "react";
+import React, { useEffect } from "react";
+import { DatePicker } from "../../../shared/layout/components/DatePicker/DatePicker";
 import Header from "../../../shared/layout/components/Header";
 import Screen from "../../../shared/layout/components/Screen";
 import Title from "../../../shared/layout/components/Title";
 import { useProfile } from "../../profile-switcher/hooks/useProfile";
+import { useSelectedSession } from "../../sessions/hooks/useSelectedSession";
 import { useSessions } from "../../sessions/hooks/useSessions";
 
 function Home() {
   const { currentUser, signOutUser } = useProfile();
-  const { sessions, isLoading, error } = useSessions(currentUser?.id);
+  const { latestSessionDate } = useSessions(currentUser?.id);
+  const { sessionData, syncSessionDataByDate } = useSelectedSession();
 
-  console.log("[Home] Response", {
-    sessions: sessions?.length,
-    isLoading,
-    error,
-  });
+  useEffect(() => {
+    console.log("sessionData", sessionData);
+  }, [sessionData]);
 
   return (
     <Screen>
@@ -22,9 +23,19 @@ function Home() {
         <Header.LeftIcon onPress={signOutUser} />
         <Header.Title>Eight Sleep</Header.Title>
       </Header.Container>
-      <Box p={"$8"}>
+      <Box px={"$5"} py={"$8"}>
         <Text testID="home-greetings">Hello {currentUser?.name},</Text>
         <Title>Sleep report</Title>
+        <Box pt="$5">
+          <DatePicker
+            currentDate={new Date()}
+            latestSessionDate={latestSessionDate}
+            onDateChange={date => syncSessionDataByDate(date)}
+          />
+        </Box>
+        <Box pt="$5">
+          {sessionData ? <Text>Tem dados</Text> : <Text>sem dados</Text>}
+        </Box>
       </Box>
     </Screen>
   );
