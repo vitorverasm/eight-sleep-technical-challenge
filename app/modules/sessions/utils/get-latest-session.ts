79e0ccde4903106e49b4302fb54c26d0003f5514
Vitor Veras <vitorverasm@gmail.com>
2024-05-12 04:43:50 -0300

    feat: create getLatestSession util function

---
+++ app/modules/sessions/utils/get-latest-session.ts
@@ -0,0 +1,13 @@
+import { SleepSession } from "../types/sleep-session";
+
+/**
+ * getLatestSession
+ *
+ * Session data are returned in descending order, so the first element in the array is the latest session.
+ *
+ * @param sessions
+ * @returns
+ */
+export function getLatestSession(sessions: SleepSession[]) {
+  return sessions[0];
+}
