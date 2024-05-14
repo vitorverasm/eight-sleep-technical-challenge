3f377aadc54b18d24b95c6319c047c5fd97d6f05
Vitor Veras <vitorverasm@gmail.com>
2024-05-14 06:24:29 -0300

    feat: add touchable around card component

--- app/shared/layout/components/Card.tsx
+++ app/shared/layout/components/Card.tsx
@@ -1,24 +1,29 @@
 import { Card as GCard } from "@gluestack-ui/themed";
 import { ComponentProps } from "react";
+import { TouchableOpacity } from "react-native";
 
 export default function Card({
   children,
   backgroundColor,
   minHeight,
   onLayout,
+  onPress,
 }: {
   children: React.ReactNode;
+  onPress?: () => void;
 } & ComponentProps<typeof GCard>) {
   return (
-    <GCard
-      size="md"
-      variant="elevated"
-      w="$full"
-      backgroundColor={backgroundColor ?? "$backgroundDark950"}
-      minHeight={minHeight}
-      onLayout={onLayout}
-    >
-      {children}
-    </GCard>
+    <TouchableOpacity onPress={onPress} activeOpacity={0.65}>
+      <GCard
+        size="md"
+        variant="elevated"
+        w="$full"
+        backgroundColor={backgroundColor ?? "$backgroundDark950"}
+        minHeight={minHeight}
+        onLayout={onLayout}
+      >
+        {children}
+      </GCard>
+    </TouchableOpacity>
   );
 }
