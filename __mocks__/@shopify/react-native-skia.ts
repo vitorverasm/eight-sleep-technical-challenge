3e2c246a69b9a40996301f56d8c3956d7921de02
Vitor Veras <vitorverasm@gmail.com>
2024-05-14 22:28:11 -0300

    test: update skia jest setup

--- __mocks__/@shopify/react-native-skia.ts
+++
@@ -1,1 +0,0 @@
-export default { useFonts: jest.fn() };
--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,9 @@ const config: Config = {
   verbose: true,
   preset: "jest-expo",
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@gluestack-ui/.*|@gluestack-style/.*|@legendapp/.*|react-native-svg)",
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@gluestack-ui/.*|@gluestack-style/.*|@legendapp/.*|react-native-svg|@shopify/react-native-skia)",
   ],
+  setupFiles: ["@shopify/react-native-skia/jestSetup.js"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
